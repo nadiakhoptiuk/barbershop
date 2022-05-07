ceab6606e96bc5023379d6a49ac2f81a5d8edd90
Nadia Khoptiuk <holineinadia@gmail.com>
2022-05-07 23:34:29 +0300

    bugfix/modal and backdrop

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,16 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtnMain: document.querySelector("[data-modal-open-main]"),
+    openModalBtnSecond: document.querySelector("[data-modal-open-second]"),
+    openModalBtnThird: document.querySelector("[data-modal-open-third]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
-    backdropRef: document.querySelector("[data-overlay]"),
+    backdropRef: document.querySelector("[data-overlay-modal]"),
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtnMain.addEventListener("click", toggleModal);
+  refs.openModalBtnSecond.addEventListener("click", toggleModal);
+  refs.openModalBtnThird.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
